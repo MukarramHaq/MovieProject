87b7e6f54828a643d69ff6109d09cde666c57529
Mukarram Haq <mukarramhaq@mukarrams-air.mynetworksettings.com>
2023-11-07 22:12:00 -0500

    commented out trailerLink

--- src/main/java/dev/mukarram/movies/Movie.java
+++ src/main/java/dev/mukarram/movies/Movie.java
@@ -40,7 +40,7 @@ public class Movie {
     private String imdbId;
     private String title;
     private String releaseDate;
-    private String trailerLink;
+    //private String trailerLink;
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
